abd232a61330431ed80f121836e4d44eee2a46a0
KyleTV <tinhvqbk@gmail.com>
2020-08-27 01:53:24 +0700

    Modal

--- frontend/src/stories/index.stories.js
+++ frontend/src/stories/index.stories.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { PageLayout,Header,Media, Logo, SafeAnchor,Badge, Button } from '@gotitinc/design-system';
-import SplitPane from 'react-split-pane';
+import React,{ useState } from 'react';
+import { PageLayout,Header,Media, Logo, SafeAnchor,Badge, Button,Modal } from '@gotitinc/design-system';
 import ChatArea from './ChatArea';
 
 export default {
   title: 'Index',
 };
 
-export const index = () => (
-  <PageLayout style={{
+export const index = () => {
+  const [show, setShow] = useState(true);
+  return(
+    <PageLayout style={{
     height: '100vh',
   }}
   >
@@ -35,13 +36,32 @@ export const index = () => (
       <div className="Container Container--fluid u-paddingTopMedium u-paddingBottomSmall u-flex u-flexColumn u-backgroundOpaline">
         <div className="Grid Grid--smallGutter u-flexGrow-1">
           <div className="u-size9of12">
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/RK1K2bCg4J8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/RK1K2bCg4J8" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
-          <div className="u-size3of12 u-flex">
+          <div className="u-size3of12 u-flex u-positionRelative">
+            {show && (
+              <div className="u-positionAbsolute u-positionFull u-zIndexModal u-flex u-flexGrow-1 u-alignItemsCenter u-justifyContentCenter">
+                <div className="Modal-backDrop u-positionAbsolute u-positionFull u-backgroundBlack u-zIndex2 Show "/>
+                <div className="u-positionRelative u-zIndex3 u-marginMedium">
+                  <Modal onHide={() => setShow(false)} size="small" relative>
+                      <Modal.Header closeButton/>
+                      <Modal.Body>
+                        <div className="u-textCenter">
+                          Chỉ được chuyển phòng tối đa 1 lần trong 5 phút. Bạn chắc chắn muốn chuyển phòng chứ?
+                        </div>
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="primary" width="full">Chuyển phòng</Button>
+                      </Modal.Footer>
+                    </Modal>
+                  </div>
+                </div>
+              )}
             <ChatArea />
           </div>
         </div>
       </div>
     </PageLayout.Body>
   </PageLayout>
-)
+  )
+}
