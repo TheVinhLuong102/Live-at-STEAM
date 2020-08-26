1213534daa971fc4abb8af0f0246ce8ad0959985
David Vu <davidvu@fb.com>
2020-08-26 15:48:50 -0700

    fix privillege check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,32 @@
 import express, { json } from 'express';
 import ChatServer from './chat_server/server';
 import jwt from 'jsonwebtoken';
-import {LocalUserManager} from './chat_server/member_manager';
+import cookieParser from 'cookie-parser';
+import UserManager, {Role} from './chat_server/member_manager';
+import {jwt_express_auth, check_admin} from './auth/jwt_auth';
 
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 type APIResponse = {
   status: number,
   response?: any,
   error?: string,
 }
-
-const user_manager = new LocalUserManager();
-
 app.post('/login', (req, res) => {
   const username: string = req.body.username;
   if(username == null) {
     return res.status(400).json({"error": "missing params"});
   }
   const token = jwt.sign({name: username}, process.env.JWT_SECRET_KEY);
-  user_manager.getState(username).then(async (userState) => {
+  UserManager.getState(username).then(async (userState) => {
     // if not registered
     if(!userState) {
-      await user_manager.addUser(username).catch((e) => {
+      await UserManager.addUser(username).catch((e) => {
         console.error(e);
         return res.status(500).json({"error": "Failed to add new user!"});
       })
@@ -35,7 +35,7 @@ app.post('/login', (req, res) => {
   });
 });
 
-app.get('/api/getRooms', (req, res) => {
+app.get('/api/getRooms',  (req, res) => {
   myChatServer.getRooms().then((rooms) =>
     res.json({
       "status": 1,
@@ -48,7 +48,7 @@ app.get('/api/getRooms', (req, res) => {
 });
 
 
-app.get('/admin/setMaxRooms', (req, res) => {
+app.get('/admin/setMaxRooms', [jwt_express_auth, check_admin], (req, res) => {
 
   if (!req.query.maxRooms) {
     return res.status(400).json({ status: -1, error: "missing params" } as APIResponse)
@@ -65,7 +65,7 @@ app.get('/admin/setMaxRooms', (req, res) => {
   } as APIResponse));
 });
 
-app.get('/admin/shuffleRooms', (req, res) => {
+app.get('/admin/shuffleRooms', [jwt_express_auth, check_admin], (req, res) => {
   myChatServer.shuffleIntoRooms().then(() =>
     res.json({
       "status": 1,
@@ -87,7 +87,7 @@ app.get('*', function(req, res) {
 
 const http_server = app.listen(process.env.PORT || 3600, () => console.log(`Server is listening on port ${process.env.PORT || 3600}`));
 
-const myChatServer = new ChatServer(http_server, user_manager);
+const myChatServer = new ChatServer(http_server, UserManager);
 myChatServer.setup();
 
 module.exports = app;
