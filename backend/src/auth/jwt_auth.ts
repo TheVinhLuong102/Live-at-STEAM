8f6f8fe2ba850fa789fa34ada53452d19d9f3ebc
David Vu <davidvu@fb.com>
2020-08-28 22:35:57 -0700

    lint

--- backend/src/auth/jwt_auth.ts
+++ backend/src/auth/jwt_auth.ts
@@ -1,49 +1,56 @@
-import jwt from 'jsonwebtoken';
-import UserManager, {Role, UserState} from "../chat_server/member_manager";
+import jwt from "jsonwebtoken";
+import UserManager, { Role, UserState } from "../chat_server/member_manager";
 
 export type DecodedUserData = {
-    user: string
-}
+  user: string;
+};
 
-export async function verifyTokenAndGetUserState(token: string): Promise<UserState>{
-    return new Promise((resolve, reject) => {
-        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, userdata: DecodedUserData) => {
-            if(err) {
-                console.error(err);
-                return reject("Failed to verify JWT Token");
-            }
-            UserManager.getState(userdata.user).then(async (userState) => {
-                // if not registered
-                if(!userState) {
-                    reject(new Error("User doens't exist"));
-                } else {
-                    resolve(userState);
-                }
-            });
+export async function verifyTokenAndGetUserState(
+  token: string
+): Promise<UserState> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET_KEY,
+      (err, userdata: DecodedUserData) => {
+        if (err) {
+          console.error(err);
+          return reject("Failed to verify JWT Token");
+        }
+        UserManager.getState(userdata.user).then(async (userState) => {
+          // if not registered
+          if (!userState) {
+            reject(new Error("User doens't exist"));
+          } else {
+            resolve(userState);
+          }
         });
-    });
+      }
+    );
+  });
 }
 
 export function check_admin(req, res, next) {
-    if(req.user.role != Role.ADMIN)
-        return res.status(401).json({"error": "Permission denied"});
-    next();
+  if (req.user.role != Role.ADMIN)
+    return res.status(401).json({ error: "Permission denied" });
+  next();
 }
 
 export function jwt_express_auth(req, res, next) {
-    const authHeader  = req.headers['authorization'];
-    const authCookie = req.cookies['live-site-jwt'];
-    const token = (authHeader && authHeader.split(" ")[1] ) || authCookie;
-    if (token == null) return res.status(403).json({"error": "No access token provided"});
+  const authHeader = req.headers["authorization"];
+  const authCookie = req.cookies["live-site-jwt"];
+  const token = (authHeader && authHeader.split(" ")[1]) || authCookie;
+  if (token == null)
+    return res.status(403).json({ error: "No access token provided" });
 
-    verifyTokenAndGetUserState(token).catch((e) => {
-        if(e instanceof String)
-            res.status(403).json({"error": e});
-        else
-            res.status(500).json({"error": "Something went wrong"});
-    }).then(userState => {
-        console.log(userState);
-        req.user = userState;
-        next();
+  verifyTokenAndGetUserState(token)
+    .catch((e) => {
+      if (e instanceof String) res.status(403).json({ error: e });
+      else res.status(500).json({ error: "Something went wrong" });
     })
+    .then((userState) => {
+      console.log(userState);
+      req.user = userState;
+      next();
+    });
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,153 +1,200 @@
-import express, { json } from 'express';
-import bodyParser from 'body-parser';
-import ChatServer from './chat_server/server';
-import jwt from 'jsonwebtoken';
-import AuthenticationServer from './auth/authentication_server';
-import cookieParser from 'cookie-parser';
-import UserManager, { Role, UserState } from './chat_server/member_manager';
-import { jwt_express_auth, check_admin, DecodedUserData} from './auth/jwt_auth';
+import express, { json } from "express";
+import bodyParser from "body-parser";
+import ChatServer from "./chat_server/server";
+import jwt from "jsonwebtoken";
+import AuthenticationServer from "./auth/authentication_server";
+import cookieParser from "cookie-parser";
+import UserManager, { Role, UserState } from "./chat_server/member_manager";
+import {
+  jwt_express_auth,
+  check_admin,
+  DecodedUserData,
+} from "./auth/jwt_auth";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(cookieParser());
 
 type APIResponse = {
-  status: number,
-  response?: any,
-  error?: string,
-}
+  status: number;
+  response?: any;
+  error?: string;
+};
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const username: string = req.body.username;
   const password: string = req.body.password;
-  if(username == null || password == null) {
-    return res.status(400).json({"error": "missing params"});
+  if (username == null || password == null) {
+    return res.status(400).json({ error: "missing params" });
   }
 
-  authenticationServer.login(username, password).then((response) => {
-    UserManager.getState(response.preferred_username).then(async (userState) => {
-      // if not registered
-      if(!userState) {
-        const res = await UserManager.addUser(response.preferred_username).catch((e) => {
-          console.error(e);
-          return res.status(500).json({"error": "Failed to add new user!"});
-        });
-        userState = res;
-      }
-      return res.json({
+  authenticationServer
+    .login(username, password)
+    .then((response) => {
+      UserManager.getState(response.preferred_username).then(
+        async (userState) => {
+          // if not registered
+          if (!userState) {
+            const res = await UserManager.addUser(
+              response.preferred_username
+            ).catch((e) => {
+              console.error(e);
+              return res.status(500).json({ error: "Failed to add new user!" });
+            });
+            userState = res;
+          }
+          return res.json({
+            status: 1,
+            access_token: jwt.sign(
+              { user: userState.username } as DecodedUserData,
+              process.env.JWT_SECRET_KEY
+            ),
+          } as APIResponse);
+        }
+      );
+    })
+    .catch((error) =>
+      res.status(400).json({
+        status: -1,
+        error,
+      } as APIResponse)
+    );
+});
+
+app.get("/api/getRooms", (req, res) => {
+  myChatServer
+    .getRooms()
+    .then((rooms) =>
+      res.json({
         status: 1,
-        access_token: jwt.sign({user: userState.username} as DecodedUserData, process.env.JWT_SECRET_KEY)
-      } as APIResponse);
-    });
-  }).catch(error => res.status(400).json({
-    status: -1,
-    error
-  } as APIResponse));
-
+        response: rooms,
+      } as APIResponse)
+    )
+    .catch((e) =>
+      res.status(500).json({
+        status: -1,
+        error: "something went wrong",
+      } as APIResponse)
+    );
 });
 
-app.get('/api/getRooms', (req, res) => {
-  myChatServer.getRooms().then((rooms) =>
-    res.json({
-      "status": 1,
-      "response": rooms
-    } as APIResponse)
-  ).catch(e => res.status(500).json({
-    status: -1,
-    error: "something went wrong"
-  } as APIResponse));
-});
-
-
-app.get('/admin/setMaxRooms', (req, res) => {
-
+app.get("/admin/setMaxRooms", (req, res) => {
   if (!req.query.maxRooms) {
-    return res.status(400).json({ status: -1, error: "missing params" } as APIResponse)
+    return res
+      .status(400)
+      .json({ status: -1, error: "missing params" } as APIResponse);
   }
   let maxRoom: number = parseInt(req.query.maxRooms as string);
-  myChatServer.setMaxNumRooms(maxRoom).then(() =>
-    res.json({
-      "status": 1,
-      "response": `Successfully set maxRooms to ${maxRoom}`
-    } as APIResponse)
-  ).catch(e => res.status(500).json({
-    status: -1,
-    error: "something went wrong"
-  } as APIResponse));
+  myChatServer
+    .setMaxNumRooms(maxRoom)
+    .then(() =>
+      res.json({
+        status: 1,
+        response: `Successfully set maxRooms to ${maxRoom}`,
+      } as APIResponse)
+    )
+    .catch((e) =>
+      res.status(500).json({
+        status: -1,
+        error: "something went wrong",
+      } as APIResponse)
+    );
 });
 
-app.get('/admin/unban', [jwt_express_auth, check_admin], (req, res) => {
+app.get("/admin/unban", [jwt_express_auth, check_admin], (req, res) => {
   if (!req.query.target_user) {
-    return res.status(400).json({ status: -1, error: "missing params" } as APIResponse)
+    return res
+      .status(400)
+      .json({ status: -1, error: "missing params" } as APIResponse);
   }
   let target_user: string = req.query.target_user;
-  UserManager.unbanUser(target_user).then(() =>
-    res.json({
-      "status": 1,
-      "response": `Successfully unbanned ${target_user}`
-    } as APIResponse)
-  ).catch(e => res.status(500).json({
-    status: -1,
-    error: "something went wrong"
-  } as APIResponse));
+  UserManager.unbanUser(target_user)
+    .then(() =>
+      res.json({
+        status: 1,
+        response: `Successfully unbanned ${target_user}`,
+      } as APIResponse)
+    )
+    .catch((e) =>
+      res.status(500).json({
+        status: -1,
+        error: "something went wrong",
+      } as APIResponse)
+    );
 });
 
-
-app.get('/admin/ban', [jwt_express_auth, check_admin], (req, res) => {
+app.get("/admin/ban", [jwt_express_auth, check_admin], (req, res) => {
   if (!req.query.target_user) {
-    return res.status(400).json({ status: -1, error: "missing params" } as APIResponse)
+    return res
+      .status(400)
+      .json({ status: -1, error: "missing params" } as APIResponse);
   }
   let target_user: string = req.query.target_user;
-  UserManager.banUser(target_user).then(() =>
-    res.json({
-      "status": 1,
-      "response": `Successfully banned ${target_user}`
-    } as APIResponse)
-  ).catch(e => res.status(500).json({
-    status: -1,
-    error: "something went wrong"
-  } as APIResponse));
+  UserManager.banUser(target_user)
+    .then(() =>
+      res.json({
+        status: 1,
+        response: `Successfully banned ${target_user}`,
+      } as APIResponse)
+    )
+    .catch((e) =>
+      res.status(500).json({
+        status: -1,
+        error: "something went wrong",
+      } as APIResponse)
+    );
 });
-
 
 /**
  * GET /api/report?target_user={username}
  */
-app.get('/api/report', [jwt_express_auth], (req, res) => {
+app.get("/api/report", [jwt_express_auth], (req, res) => {
   if (!req.query.target_user) {
-    return res.status(400).json({ status: -1, error: "missing params" } as APIResponse)
+    return res
+      .status(400)
+      .json({ status: -1, error: "missing params" } as APIResponse);
   }
   let target_user: string = req.query.target_user;
   UserManager.reportUser(target_user).then(() => {
-    return res.json({ status: 1, response: `User ${target_user} has been reported.` } as APIResponse);
+    return res.json({
+      status: 1,
+      response: `User ${target_user} has been reported.`,
+    } as APIResponse);
   });
 });
 
-app.get('/admin/shuffleRooms',  (req, res) => {
-  myChatServer.shuffleIntoRooms().then(() =>
-    res.json({
-      "status": 1,
-      "response": `Successfully shuffled clients to different rooms`
-    } as APIResponse)
-  ).catch(e => res.status(500).json({
-    status: -1,
-    error: "something went wrong"
-  } as APIResponse));
+app.get("/admin/shuffleRooms", (req, res) => {
+  myChatServer
+    .shuffleIntoRooms()
+    .then(() =>
+      res.json({
+        status: 1,
+        response: `Successfully shuffled clients to different rooms`,
+      } as APIResponse)
+    )
+    .catch((e) =>
+      res.status(500).json({
+        status: -1,
+        error: "something went wrong",
+      } as APIResponse)
+    );
 });
 
 // we build frontend app to the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // fallback URL to redirect other requests to react app
-app.get('*', function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile("/public/index.html", { root: __dirname });
 });
 
-const http_server = app.listen(process.env.PORT || 3600, () => console.log(`Server is listening on port ${process.env.PORT || 3600}`));
+const http_server = app.listen(process.env.PORT || 3600, () =>
+  console.log(`Server is listening on port ${process.env.PORT || 3600}`)
+);
 
 const myChatServer = new ChatServer(http_server, UserManager);
 myChatServer.setup();
