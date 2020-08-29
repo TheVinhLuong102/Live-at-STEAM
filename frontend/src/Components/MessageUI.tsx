b3362828149a23d8d5c63edb47e578ec4636e2e1
David Vu <davidvu98@gmail.com>
2020-08-29 13:28:38 -0700

    add linkify to be able to embed hyperlink

--- frontend/src/Components/MessageUI.tsx
+++ frontend/src/Components/MessageUI.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Linkify from "react-linkify";
 
 import {
   Avatar,
@@ -11,122 +12,129 @@ import {
 import classNames from "classnames";
 import { useUserData } from "../Hooks/User";
 import { useSocket } from "../Hooks/Socket";
-import DropdownWrapper from './DropdownWrapper';
-import { getShortName, chooseColorByString } from '../Utils/Common';
+import DropdownWrapper from "./DropdownWrapper";
+import { getShortName, chooseColorByString } from "../Utils/Common";
 
 export function UserMessageUI({
-    username,
-    message,
-    messageId,
-    message_type,
-    socket,
-  }: {
-    username: string,
-    message: string,
-    messageId: string
-    message_type: string,
-    socket: SocketIOClient.Socket | null | undefined
-  }) {
-    const [hover, setHover] = React.useState(false);
-    const userData = useUserData();
-  
-    const handleDeleteMessage = () => {
-      socket?.emit("delete_message", messageId);
-    }
+  username,
+  message,
+  messageId,
+  message_type,
+  socket,
+}: {
+  username: string;
+  message: string;
+  messageId: string;
+  message_type: string;
+  socket: SocketIOClient.Socket | null | undefined;
+}) {
+  const [hover, setHover] = React.useState(false);
+  const userData = useUserData();
 
-    const handleReportUser = (username: string) => {
-      socket?.emit("report", username);
-    }
-  
-    return (
-      <div
-        className="u-flex u-marginBottomExtraSmall u-paddingRightMedium u-positionRelative"
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-        <div className="u-flexShrink-0 u-marginRightExtraSmall">
-          {/* <Avatar src={require("../assets/images/kid-boy.png")} /> */}
-          <Avatar
-            className={classNames(
-              "u-text200 u-textUppercase",
-              chooseColorByString(username),
-            )}
-            text={getShortName(username)}
-          />
-        </div>
-        <div className="u-flexGrow-1 u-text200 u-marginTopTiny u-textWordBreak">
-          <span
-            className={classNames(
-              "u-fontBold u-textLight"
-            )}
-          >
-            {username}
-          </span>
-          {message_type === "global" && (
-            <Badge variant="positive" className="u-marginLeftTiny u-fontRegular u-text100">admin</Badge>
+  const handleDeleteMessage = () => {
+    socket?.emit("delete_message", messageId);
+  };
+
+  const handleReportUser = (username: string) => {
+    socket?.emit("report", username);
+  };
+
+  return (
+    <div
+      className="u-flex u-marginBottomExtraSmall u-paddingRightMedium u-positionRelative"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className="u-flexShrink-0 u-marginRightExtraSmall">
+        {/* <Avatar src={require("../assets/images/kid-boy.png")} /> */}
+        <Avatar
+          className={classNames(
+            "u-text200 u-textUppercase",
+            chooseColorByString(username)
           )}
-          <span
-            className={classNames(
-              "u-marginLeftExtraSmall",
-              message_type === "global" && "u-textPositive"
+          text={getShortName(username)}
+        />
+      </div>
+      <div className="u-flexGrow-1 u-text200 u-marginTopTiny u-textWordBreak">
+        <span className={classNames("u-fontBold u-textLight")}>{username}</span>
+        {message_type === "global" && (
+          <Badge
+            variant="positive"
+            className="u-marginLeftTiny u-fontRegular u-text100"
+          >
+            admin
+          </Badge>
+        )}
+        <span
+          className={classNames(
+            "u-marginLeftExtraSmall",
+            message_type === "global" && "u-textPositive"
+          )}
+        >
+          <Linkify
+            componentDecorator={(href, text, key) => (
+              <a href={href} key={key} target="_blank">
+                {text}
+              </a>
             )}
           >
             {message}
-          </span>
-        </div>
-        {hover && (
-          <div className="u-positionAbsolute u-positionRight u-positionTop">
-            <DropdownWrapper
-              renderer={(showDropdown: boolean, setShowDropdown: Function) => (
-                <Dropdown
-                  alignRight
-                  onToggle={() => setShowDropdown(!showDropdown)}
-                  show={showDropdown}
+          </Linkify>
+        </span>
+      </div>
+      {hover && (
+        <div className="u-positionAbsolute u-positionRight u-positionTop">
+          <DropdownWrapper
+            renderer={(showDropdown: boolean, setShowDropdown: Function) => (
+              <Dropdown
+                alignRight
+                onToggle={() => setShowDropdown(!showDropdown)}
+                show={showDropdown}
+              >
+                <Dropdown.Toggle className="u-textLight hover:u-textGray u-lineHeightNone u-rotate90">
+                  <Icon size="extraSmall" name="more" />
+                </Dropdown.Toggle>
+                <Dropdown.Container
+                  className="u-paddingVerticalExtraSmall"
+                  additionalStyles={{ minWidth: 150 }}
                 >
-                  <Dropdown.Toggle className="u-textLight hover:u-textGray u-lineHeightNone u-rotate90">
-                    <Icon size="extraSmall" name="more" />
-                  </Dropdown.Toggle>
-                  <Dropdown.Container
-                    className="u-paddingVerticalExtraSmall"
-                    additionalStyles={{ minWidth: 150 }}
+                  <Dropdown.Item
+                    className="u-cursorPointer u-alignItemsCenter"
+                    role="button"
+                    onClick={() => {
+                      handleReportUser(username);
+                      setShowDropdown(false);
+                    }}
                   >
+                    <Icon name="flag" size="extraSmall" />
+                    <span className="u-marginLeftExtraSmall u-text200 u-textNoWrap">
+                      Báo cáo vi phạm
+                    </span>
+                  </Dropdown.Item>
+                  {userData?.role === 0 && (
                     <Dropdown.Item
                       className="u-cursorPointer u-alignItemsCenter"
                       role="button"
                       onClick={() => {
-                        handleReportUser(username)
+                        handleDeleteMessage();
                         setShowDropdown(false);
                       }}
                     >
-                      <Icon name="flag" size="extraSmall" />
+                      <Icon name="trash" size="extraSmall" />
                       <span className="u-marginLeftExtraSmall u-text200 u-textNoWrap">
-                        Báo cáo vi phạm
+                        Xoá tin nhắn
                       </span>
                     </Dropdown.Item>
-                    {userData?.role === 0 && (
-                      <Dropdown.Item
-                        className="u-cursorPointer u-alignItemsCenter"
-                        role="button"
-                        onClick={() => {
-                          handleDeleteMessage();
-                          setShowDropdown(false);
-                        }}
-                      >
-                        <Icon name="trash" size="extraSmall" />
-                        <span className="u-marginLeftExtraSmall u-text200 u-textNoWrap">
-                          Xoá tin nhắn
-                        </span>
-                      </Dropdown.Item>
-                    )}
-                  </Dropdown.Container>
-                </Dropdown>
-              )}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
+                  )}
+                </Dropdown.Container>
+              </Dropdown>
+            )}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
 
 export function SystemMessageUI({
   message,
